82ca0f818a734c770f2a1f1f91288325126f2a40
Garrett Pace <elderpace6@gmail.com>
2023-05-08 15:39:33 -0600

    peopleAdded

--- sql-basics/person.sql
+++ sql-basics/person.sql
@@ -6,3 +6,11 @@ CREATE TABLE person(
   city VARCHAR(30),
   favorite_color VARCHAR(20)
 );
+
+INSERT INTO people (name, age, height_cm, city, favorite_color)
+VALUES ('Bob', 34, 180, 'Las Vegas', 'blue'),
+('Sally', 25, 151, 'Las Vegas', 'red'),
+('Allen', 19, 256, 'Mars', 'green'),
+('Dan', 50, 167, 'Provo', 'black'),
+('Amy', 43, 142, 'Salt Lake', 'blue');
+
