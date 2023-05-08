c7c88dbf19b43dfc8453a88ff043b3c6811ad142
Garrett Pace <elderpace6@gmail.com>
2023-05-08 16:23:28 -0600

    step2done

--- sql-basics/person.sql
+++ sql-basics/person.sql
@@ -1,11 +1,11 @@
-CREATE TABLE person(
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(25) NOT NULL,
-  age INTEGER,
-  height_cm INTEGER,
-  city VARCHAR(30),
-  favorite_color VARCHAR(20)
-);
+-- CREATE TABLE person(
+--   id SERIAL PRIMARY KEY,
+--   name VARCHAR(25) NOT NULL,
+--   age INTEGER,
+--   height_cm INTEGER,
+--   city VARCHAR(30),
+--   favorite_color VARCHAR(20)
+-- );
 
 INSERT INTO people (name, age, height_cm, city, favorite_color)
 VALUES ('Bob', 34, 180, 'Las Vegas', 'blue'),
@@ -13,4 +13,30 @@ VALUES ('Bob', 34, 180, 'Las Vegas', 'blue'),
 ('Allen', 19, 256, 'Mars', 'green'),
 ('Dan', 50, 167, 'Provo', 'black'),
 ('Amy', 43, 142, 'Salt Lake', 'blue');
+
+SELECT * FROM person ORDER BY height_cm DESC;
+
+SELECT * FROM person ORDER BY height_cm ASC;
+
+SELECT * FROM person ORDER BY age DESC;
+
+SELECT * FROM person WHERE age > 20;
+
+SELECT * FROM person WHERE age = 18;
+
+SELECT * FROM person WHERE age < 20 OR age > 30;
+
+SELECT * FROM person WHERE age <> 27;
+
+SELECT * FROM person WHERE favorite_color <> 'red';
+
+SELECT * FROM person WHERE favorite_color <> 'red' AND favorite_color <> 'blue';
+
+SELECT * FROM person WHERE favorite_color = 'orange' OR favorite_color = 'green';
+
+SELECT * FROM person WHERE favorite_color IN ('orange', 'green', 'blue');
+
+SELECT * FROM person WHERE favorite_color IN ('yellow', 'purple');
+
+
 
